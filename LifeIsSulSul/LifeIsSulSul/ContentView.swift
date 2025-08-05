deeaacb5974fe789cf4651b018e179d957e9f2ab
ParkSY0919 <qhr498@gmail.com>
2025-08-05 19:19:59 +0900

    [Setting/#1] 프로젝트 생성 및 이미지 리소스 추가 등

---
+++ LifeIsSulSul/LifeIsSulSul/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeIsSulSul
+//
+//  Created by 박신영 on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
